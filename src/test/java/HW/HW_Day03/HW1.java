3578a6489054ef9a9ba8e93cb27f5154bd23d03e
AJHG059 <abdulaziz.jameel.alghamdi@gmail.com>
2024-04-25 18:28:19 +0300

    update

--- src/test/java/HW/HW_Day03/HW1.java
+++ src/test/java/HW/HW_Day03/HW1.java
@@ -15,49 +15,50 @@ import static org.testng.Assert.assertNotNull;
 public class HW1 extends JsonPlaseHolderBaseUrl {
 
 
-     /*
-        Given
-            1) https://reqres.in/api/users
-            2) {
-                "name": "morpheus",
-                "job": "leader"
-                }
-        When
-            I send POST Request to the Url
-        Then
-            Status code is 201
-            And response body should be like {
-                                                "name": "morpheus",
-                                                "job": "leader",
-                                                "id": "496",
-                                                "createdAt": "2022-10-04T15:18:56.372Z"
-                                              }
-     */
-        @Test
-        public void testPostRequest () {
-            // Given
-            String url = "https://reqres.in/api/users";
-            Map<String, String> requestBody = Map.of(
-                    "name", "morpheus",
-                    "job", "leader"
-            );
+    /*
+       Given
+           1) https://reqres.in/api/users
+           2) {
+               "name": "morpheus",
+               "job": "leader"
+               }
+       When
+           I send POST Request to the Url
+       Then
+           Status code is 201
+           And response body should be like {
+                                               "name": "morpheus",
+                                               "job": "leader",
+                                               "id": "496",
+                                               "createdAt": "2022-10-04T15:18:56.372Z"
+                                             }
+    */
+    @Test
+    public void testPostRequest() {
+        // Given
+        String url = "https://reqres.in/api/users";
+        Map<String, String> requestBody = Map.of(
+                "name", "morpheus",
+                "job", "leader"
+        );
 
-            // When
-            Response response = RestAssured.given()
-                    .contentType(ContentType.JSON)
-                    .body(requestBody)
-                    .post(url);
+        // When
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(url);
 
-            // Then
-            assertEquals(response.statusCode(), 201);
+        response.prettyPrint();
+        // Then
+        assertEquals(response.statusCode(), 201);
 
-            Map<String, Object> responseBody = response.body().as(Map.class);
-            assertNotNull(responseBody.get("id"));
-            assertNotNull(responseBody.get("createdAt"));
-            assertEquals(responseBody.get("name"), "morpheus");
-            assertEquals(responseBody.get("job"), "leader");
-        }
+        Map<String, Object> responseBody = response.body().as(Map.class);
+        assertNotNull(responseBody.get("id"));
+        assertNotNull(responseBody.get("createdAt"));
+        assertEquals(responseBody.get("name"), "morpheus");
+        assertEquals(responseBody.get("job"), "leader");
     }
+}
 
 
 
